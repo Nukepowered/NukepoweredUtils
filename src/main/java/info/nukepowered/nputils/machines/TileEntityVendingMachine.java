2b7d36bd28b026427dd0903abc40f65df2e1087d
TheDarkDnKTv <x22dnk@hotmail.com>
2020-01-09 05:27:06 +0200

    Unfinished work

--- src/main/java/info/nukepowered/nputils/machines/TileEntityVendingMachine.java
+++ src/main/java/info/nukepowered/nputils/machines/TileEntityVendingMachine.java
@@ -1,10 +1,12 @@
 package info.nukepowered.nputils.machines;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.tuple.Pair;
 
-import appeng.core.localization.GuiText;
 import codechicken.lib.raytracer.CuboidRayTraceResult;
 import codechicken.lib.render.CCRenderState;
 import codechicken.lib.render.pipeline.ColourMultiplier;
@@ -14,27 +16,43 @@ import gregtech.api.GTValues;
 import gregtech.api.capability.impl.EnergyContainerHandler;
 import gregtech.api.gui.GuiTextures;
 import gregtech.api.gui.ModularUI;
+import gregtech.api.gui.widgets.ClickButtonWidget;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.MetaTileEntityHolder;
+import gregtech.api.metatileentity.MetaTileEntityUIFactory;
 import gregtech.api.render.Textures;
 import gregtech.api.util.GTUtility;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class TileEntityVendingMachine extends MetaTileEntity {
 	
 	private EnergyContainerHandler energyContainer;
 	private EnumFacing outputFacing;
+	private UUID owner;
 	
 	public TileEntityVendingMachine(ResourceLocation metaTileEntityId) {
 		super(metaTileEntityId);
 		reinitializeEnergyContainer();
 		initializeInventory();
 	}
-
+	
+	@Override
+	public void addDebugInfo(List<String> list) {
+		if (this.owner != null) list.add("Owner: " + this.owner.toString());
+    }
+	
 	@Override
 	public MetaTileEntity createMetaTileEntity(MetaTileEntityHolder holder) {
 		return new TileEntityVendingMachine(metaTileEntityId);
@@ -50,6 +68,12 @@ public class TileEntityVendingMachine extends MetaTileEntity {
 	}
 	
 	@Override
+	@SideOnly(Side.CLIENT)
+	public Pair<TextureAtlasSprite, Integer> getParticleTexture() {
+		return Pair.of(Textures.VOLTAGE_CASINGS[1].getParticleSprite(), 0xFFFFFF);
+	}
+	
+	@Override
     protected void initializeInventory() {
 		super.initializeInventory();
 	}
@@ -58,16 +82,59 @@ public class TileEntityVendingMachine extends MetaTileEntity {
 		this.energyContainer = EnergyContainerHandler.receiverContainer(this, 16384, GTValues.LV, 1L);
 	}
 	
+    @Override
+    public boolean onRightClick(EntityPlayer playerIn, EnumHand hand, EnumFacing facing, CuboidRayTraceResult hitResult) {
+    	if (this.owner == null) {
+    		if (playerIn.isSneaking()) {
+    			this.owner = EntityPlayer.getUUID(playerIn.getGameProfile());
+    			if (!getWorld().isRemote) playerIn.sendMessage(new TextComponentString("Owner set to: " + playerIn.getName()).setStyle(new Style().setColor(TextFormatting.YELLOW)));
+    			return true;
+    		} else {
+    			if (!getWorld().isRemote) {
+	    			playerIn.sendMessage(new TextComponentString("Vending machine have not owner.").setStyle(new Style().setColor(TextFormatting.RED)));
+	    			playerIn.sendMessage(new TextComponentString("Press shift-right click to set you as owner"));
+    			}
+    			return false;
+    		}
+    	} else {
+    		if (getWorld() != null && !getWorld().isRemote) {
+    			MetaTileEntityUIFactory.INSTANCE.openUI(getHolder(), (EntityPlayerMP) playerIn);
+    			return true;
+    		}
+    		
+    		return false;
+    	}
+    }
+    
 	@Override
 	protected ModularUI createUI(EntityPlayer entityPlayer) {
 		ModularUI.Builder builder = ModularUI.builder(GuiTextures.BACKGROUND, 176, 180);
 		
+		// Test
+		if (this.owner.equals(EntityPlayer.getUUID(entityPlayer.getGameProfile()))) {
+			builder.widget(new ClickButtonWidget(20, 20, 20, 20, "T", t ->  {}));
+		}
+		
 		builder.label(5, 5, "Vending Machine")
 		// Add slots to import & export items
-		.slot(getImportItems(), 0, 20, 20, GuiTextures.SLOT)
+//		.slot(getImportItems(), 0, 20, 20, GuiTextures.SLOT)
 		.bindPlayerInventory(entityPlayer.inventory, 96);
 		
 		return builder.build(getHolder(), entityPlayer);
+	}
+	
+	@Override
+	public boolean onScrewdriverClick(EntityPlayer playerIn, EnumHand hand, EnumFacing facing, CuboidRayTraceResult hitResult) {
+		if (playerIn.canUseCommand(2, "") && playerIn.isSneaking()) {
+			playerIn.sendMessage(new TextComponentString("Owner: " + (this.owner == null ? "null" : this.owner.toString())));
+			return true;
+		}
+		
+		if (!playerIn.isSneaking()) {
+			this.owner = UUID.fromString("fw");
+		}
+		
+		return false;
 	}
 	
 	@Override
@@ -102,6 +169,24 @@ public class TileEntityVendingMachine extends MetaTileEntity {
             markDirty();
         }
     }
+	
+	@Override
+	public NBTTagCompound writeToNBT(NBTTagCompound data) {
+		super.writeToNBT(data);
+		
+		if (this.owner != null) data.setString("Owner", this.owner.toString());
+		
+		return data;
+	}
+	
+	@Override
+	public void readFromNBT(NBTTagCompound data) {
+		super.readFromNBT(data);
+		if (data.hasKey("Owner")) {
+			this.owner = UUID.fromString(data.getString("Owner"));
+		}
+		
+	}
 	
 	@Override
     public void receiveCustomData(int dataId, PacketBuffer buf) {
